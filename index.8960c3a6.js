94f506edd50a68bcd61458d1b14b211cd63812a9
Fetrw <21vladmel11@gmail.com>
2024-08-14 17:47:14 +0300

    make build

---
+++ index.8960c3a6.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");t.addEventListener("click",function(c){var i=c.offsetX,n=c.offsetY,a=t.clientWidth,l=t.clientHeight,o=e.clientWidth,h=e.clientHeight;e.style.left="".concat(Math.max(0,Math.min(i-o/2,a-o)),"px"),e.style.top="".concat(Math.max(0,Math.min(n-h/2,l-h)),"px")});
+//# sourceMappingURL=index.8960c3a6.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+wall.addEventListener("click", (e)=>{
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const wallWidth = wall.clientWidth;
+    const wallHeight = wall.clientHeight;
+    const spiderWidth = spider.clientWidth;
+    const spiderHeight = spider.clientHeight;
+    const newLeft = Math.max(0, Math.min(x - spiderWidth / 2, wallWidth - spiderWidth));
+    const newTop = Math.max(0, Math.min(y - spiderHeight / 2, wallHeight - spiderHeight));
+    spider.style.left = `${newLeft}px`;
+    spider.style.top = `${newTop}px`;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
